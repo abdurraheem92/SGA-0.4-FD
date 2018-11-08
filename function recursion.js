bd15a4c72a4025d7f845ff0618a08875868eb96e
abdurraheem92 <40738450+abdurraheem92@users.noreply.github.com>
2018-11-08 08:12:54 +0100

    Delete function recursion.js

--- function recursion.js
+++
@@ -1,9 +0,0 @@
-var isEven = function(num) {
-    num = Math.abs(num); //convert to absolute value to account for negative numbers
-    if (num === 0)
-        return true;
-    else if (num === 1)
-        return false;
-    else
-        return isEven(num - 2);  // recurse for numbers that are not 0 or 1
-};
